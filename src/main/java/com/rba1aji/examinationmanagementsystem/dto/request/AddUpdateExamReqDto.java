4f0182c948b4e48d5b3cb617d32171a485daf7c0
Balaji <rjibalaji@gmail.com>
2023-11-20 00:39:54 +0530

    Fix saveupdate exam semester

--- src/main/java/com/rba1aji/examinationmanagementsystem/dto/request/AddUpdateExamReqDto.java
+++ src/main/java/com/rba1aji/examinationmanagementsystem/dto/request/AddUpdateExamReqDto.java
@@ -4,9 +4,9 @@ import lombok.Data;
 
 @Data
 public class AddUpdateExamReqDto {
-  private String id;
+  private int id;
   private String name;
-  private String semester;
+  private int semester;
   private String batch;
   private String departments;
 }
