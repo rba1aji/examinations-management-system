a8e53ee13ab7839eb4640e4950911ecc9d25084c
Balaji_R <rjibalaji@gmail.com>
2023-11-24 13:31:30 +0530

    Add health check api, change db

--- src/main/java/com/rba1aji/examinationmanagementsystem/controller/AdminController.java
+++ src/main/java/com/rba1aji/examinationmanagementsystem/controller/AdminController.java
@@ -1,8 +1,15 @@
 package com.rba1aji.examinationmanagementsystem.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AdminController {
   //ToDo: Access levels to users, permit, disable
+
+  @GetMapping
+  public String home() {
+    return "IM ALIVE";
+  }
+
 }
